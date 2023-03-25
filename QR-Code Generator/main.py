959f65c0fd8f718eaa8764f9e58945134a36f83b
Sahil Sharma <85671637+Sam6900@users.noreply.github.com>
2023-03-25 16:19:47 +0530

    Add api request

--- QR-Code Generator/main.py
+++ QR-Code Generator/main.py
@@ -1,1 +1,40 @@
+import requests
+from io import BytesIO
+import PIL
 
+url = "https://qrcode3.p.rapidapi.com/qrcode/text"
+
+payload = {
+    "data": "https://linqr.app",
+    "image": {
+        "uri": "icon://appstore",
+        "modules": True
+    },
+    "style": {
+        "module": {
+            "color": "black",
+            "shape": "default"
+        },
+        "inner_eye": {"shape": "default"},
+        "outer_eye": {"shape": "default"},
+        "background": {}
+    },
+    "size": {
+        "width": 200,
+        "quiet_zone": 4,
+        "error_correction": "M"
+    },
+    "output": {
+        "filename": "qrcode",
+        "format": "png"
+    }
+}
+
+headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "YOUR_API_KEY",
+    "X-RapidAPI-Host": "qrcode3.p.rapidapi.com"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+print(response)
